c948d947719f51d795be7b5a7895a28d3aebf70c
Amanda <amandarenee195@gmail.com>
2026-01-07 11:27:39 +0300

    Add Header, MainContent, and Footer components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -5,11 +5,19 @@ import './App.css'
 
 import WelcomeMessage from './components/WelcomeMessage';
 
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Header />
+      <MainContent />
+      <Footer />
+      
     <WelcomeMessage />
 
       <div>
