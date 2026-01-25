55bc2cc246f36063a9ebe0ade077ce21fe533c5a
Amanda <amandarenee195@gmail.com>
2026-01-25 09:30:59 +0300

    Add Counter component using useState hook

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,3 +1,4 @@
+
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
@@ -9,20 +10,22 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile';
+import Counter from './components/Counter';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <UserProfile
-  name="Alice"
-  age="25"
-  bio="Loves hiking and photography"
-/>
-
-    <Header />
+      <Header />
       <MainContent />
+      <UserProfile
+        name="Alice"
+        age="25"
+        bio="Loves hiking and photography"
+      />
+      <Counter />
       <Footer />
 
     <WelcomeMessage />
