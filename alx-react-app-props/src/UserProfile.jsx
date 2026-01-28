ffed63a1aa35fa5f337ab61ba5380b78ecad0afc
Amanda <amandarenee195@gmail.com>
2026-01-28 14:19:21 +0300

    Fix UserContext import for Context API

--- alx-react-app-props/src/UserProfile.jsx
+++ alx-react-app-props/src/UserProfile.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
-import { UserContext } from './UserContext';
+import UserContext from './UserContext';
+
 
 function UserProfile() {
   const userData = useContext(UserContext);
