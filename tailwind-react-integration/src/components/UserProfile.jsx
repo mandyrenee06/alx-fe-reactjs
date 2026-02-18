20e0a5389165fbcd1fd19fe6158e499cda96691f
Mandy Renee <amandarenee195@gmail.com>
2026-02-18 18:58:39 +0300

    Update UserProfile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,7 +4,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="rounded-full w-[150px] h-[150px] mx-auto"
+        className="rounded-full w-36 h-36 mx-auto"
       />
       <h1 className="text-xl text-blue-800 my-4 text-center">
         John Doe
