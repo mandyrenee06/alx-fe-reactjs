715180f29e05f146769536f81015c5556a1bac83
Amanda <amandarenee195@gmail.com>
2026-02-19 11:46:20 +0300

    Add hover effects and transistions to UserProfile

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,7 +5,9 @@ function UserProfile() {
                     max-w-xs md:max-w-sm 
                     mx-auto 
                     rounded-lg 
-                    shadow-lg">
+                    shadow-lg 
+                    transition-shadow duration-300 ease-in-out
+                    hover:shadow-xl">
 
       <img
         src="https://via.placeholder.com/150"
@@ -13,14 +15,18 @@ function UserProfile() {
         className="rounded-full 
                    sm:w-24 sm:h-24 
                    md:w-36 md:h-36 
-                   mx-auto"
- 
+                   mx-auto
+                   transform 
+                   transition-transform duration-300 ease-in-out
+                   hover:scale-110"
       />
 
       <h1 className="text-lg md:text-xl 
                      text-blue-800 
                      my-4 
-                     text-center">
+                     text-center
+                     transition-colors duration-300
+                     hover:text-blue-500">
         John Doe
       </h1>
 
